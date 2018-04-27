eda2125cb40cb7c0e5759e21c6d1c50b692e2583
jescalaya <jescalaya@asistplantapdc.ADR.PE>
2018-04-27 14:01:10 -0500

    d

--- src/BaseDeDatos/D_RecepcionFruta.java
+++ src/BaseDeDatos/D_RecepcionFruta.java
@@ -22,11 +22,11 @@ public class D_RecepcionFruta implements ICrud<E_RecepcionFruta> {
     //private final String INSERTAR = "INSERT INTO RECEPCION_FRUTA (idLote, idEstado,rFrut_Fech_Pesa, rFrut_Fech_Proc, rFrut_Guia_Remis, rFrut_Viaje, rFrut_Tara, rFrut_Peso_Bruto,"
      //       + " rFrut_Canti, rFrut_Peso, rFrut_PTota, rFrut_PNeto, rFrut_Obser, rFrut_esReproceso, rFrut_esProcesado) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
     private final String ACTUALIZAR = "UPDATE RECEPCION_FRUTA SET idLote = ?, rFrut_CodBar = ?, rFrut_Fech_Pesa = ?,  rFrut_Guia_Remis = ?, rFrut_Viaje = ?, rFrut_Tara = ?, rFrut_Peso_Bruto = ?,  rFrut_Canti = ?, rFrut_Peso = ?,rFrut_PTota = ?,rFrut_PNeto = ?, rFrut_Obser = ?, rFrut_esReproceso = ?, rFrut_esProcesado = ?";
-    private final String MOSTRAR = "SELECT rf.idRecepcion, lo.lote_NCort, rf.rFrut_Fech_Pesa, rf.rFrut_Fech_Proc, rf.rFrut_Guia_Remis, rf.rFrut_Viaje, rf.rFrut_Peso_Bruto, rf.rFrut_Canti, rf.rFrut_Tara, rf.rFrut_Peso, rf.rFrut_PTota, rf.rFrut_PNeto\n" +
-                                    "FROM RECEPCION_FRUTA AS rf \n" +
+    //private final String MOSTRAR = "SELECT rf.idRecepcion, lo.lote_NCort, rf.rFrut_Fech_Pesa, rf.rFrut_Fech_Proc, rf.rFrut_Guia_Remis, rf.rFrut_Viaje, rf.rFrut_Peso_Bruto, rf.rFrut_Canti, rf.rFrut_Tara, rf.rFrut_Peso, rf.rFrut_PTota, rf.rFrut_PNeto\n" +
+    /*                                "FROM RECEPCION_FRUTA AS rf \n" +
                                     "INNER JOIN LOTE AS lo ON rf.idLote = lo.idLote \n" +
                                     "INNER JOIN ESTADO AS es ON rf.idEstado = es.idEstado";
-
+*/
     private Conexion instancia = Conexion.getInstancia();
 
     private PreparedStatement pstm;
